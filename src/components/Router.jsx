d0ca0b5a0642811108b5d84967a312e06439d28f
Manon <manonlefkos@gmail.com>
2023-10-15 22:11:55 +0200

    Add router

---
+++ src/components/Router.jsx
@@ -0,0 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "./Home";
+import Shop from "./Shop";
+import Cart from "./Cart";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "shop",
+          element: <Shop />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
