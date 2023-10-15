2889ecfb4b36917f9ca10cfbb38e2aa1ca525538
Manon <manonlefkos@gmail.com>
2023-10-15 17:02:59 +0200

    add Header dummy setup including tests

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  console.log("rendering Header Component");
+
+  return (
+    <>
+      <header>
+        <div>logo</div>
+        <div>
+          <a href="">home</a>
+          <a href="">shop</a>
+          <a href="">cart</a>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
