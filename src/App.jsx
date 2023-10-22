dca72bc4d4bda93f2dd8e76c5b0a2ba69c2f1c7d
Manon <manonlefkos@gmail.com>
2023-10-22 21:56:30 +0200

    add data Fetching module

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,31 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { createContext, useState } from "react";
+import FetchData from "./components/FetchData";
 
-export const ShopContext = createContext({
-  products: [],
-  cartItems: [],
-  addToCart: () => {},
-});
+export const ShopContext = createContext();
 
 const App = () => {
   console.log("rendering App component");
 
   const [cartItems, setCartItems] = useState([]);
-  const products = [];
 
   const addToCart = () => {
     setCartItems(cartItems + 1);
   };
 
+  // API Fetch
+  const url =
+    "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}";
+  // data will contain the shop products
+  const { data, loading, error } = FetchData(url);
+
+ 
+  console.log(data);
+
   return (
-    <ShopContext.Provider value={{ cartItems, products, addToCart }}>
+    <ShopContext.Provider
+      value={{ cartItems, data, addToCart, loading, error }}>
       <Header />
       <Outlet />
       <Footer />
---
+++ src/components/FetchData.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const FetchData = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setError(data.error);
+        setData(data.data.products.edges);
+        setLoading(false);
+      });
+    // json.data.products.edges
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default FetchData
