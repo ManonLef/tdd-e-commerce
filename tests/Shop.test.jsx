8d3527079bbcd32bc7b9b9c7b91cf09eb7e6d4f7
Manon <manonlefkos@gmail.com>
2023-10-22 22:02:25 +0200

    add test for loading content

--- tests/Shop.test.jsx
+++ tests/Shop.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Shop from "../src/components/Shop";
 import { ShopContext } from "../src/App";
 
-const mockErrorRender = (data = [], loading = false, error = 200) =>
+const customRender = (data = [], loading = false, error = 200) =>
   render(
     <ShopContext.Provider
       value={{
@@ -17,7 +17,12 @@ const mockErrorRender = (data = [], loading = false, error = 200) =>
 
 describe("Shop Component", () => {
   it("displays error 400 when fetch returns error 400", () => {
-    mockErrorRender([], false, 400)
+    customRender([], false, 400)
     expect(screen.getByText(/error 400/i)).toBeInTheDocument();
+  });
+
+  it("displays loading when data isn't fetched", () => {
+    customRender([], true, null)
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
   });
 });
