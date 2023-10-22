91999b3b2dec8f5b67c2da32fe44202017937cfc
Manon <manonlefkos@gmail.com>
2023-10-22 21:56:47 +0200

    add shop content for error and loading including test

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,14 @@
+import { useContext } from "react"
+import { ShopContext } from "../App"
+
 const Shop = () => {
+  // get shop fetched data from Shopping API
+  const { data, loading, error } = useContext(ShopContext);
+  console.log(data)
+  
+  if (loading) return <div className="flex-1">loading</div>;
+  if (error) return <div className="flex-1">error {error}</div>;
+  
   return (
     <div>hi from Shop</div>
   )
--- tests/Shop.test.jsx
+++ tests/Shop.test.jsx
@@ -1,10 +1,23 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Shop from "../src/components/Shop";
+import { ShopContext } from "../src/App";
+
+const mockErrorRender = (data = [], loading = false, error = 200) =>
+  render(
+    <ShopContext.Provider
+      value={{
+        data: data,
+        loading: loading,
+        error: error,
+      }}>
+      <Shop />
+    </ShopContext.Provider>
+  );
 
 describe("Shop Component", () => {
-  it("renders dummy content", () => {
-    render(<Shop />)
-    expect(screen.getByText(/hi from shop/i)).toBeInTheDocument();
-  })
-})
+  it("displays error 400 when fetch returns error 400", () => {
+    mockErrorRender([], false, 400)
+    expect(screen.getByText(/error 400/i)).toBeInTheDocument();
+  });
+});
