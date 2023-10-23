59649033ce54f8aa8e79937668fa367b95ec249b
Manon <manonlefkos@gmail.com>
2023-10-23 13:16:01 +0200

    edit amount state for cart to 1

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../App";
 
 const ProductCard = ({ ...props }) => {
   const { addToCart } = useContext(ShopContext);
-  const [input, setInput] = useState(null)
+  const [input, setInput] = useState(1)
 
   function inputHandler(e) {
     e.preventDefault()
@@ -25,7 +25,7 @@ const ProductCard = ({ ...props }) => {
           type="number"
           name="amount"
           id="amount"
-          min="0"
+          min="1"
           className="my-2 px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="1"
           onChange={inputHandler}
