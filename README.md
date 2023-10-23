921976f15284b16b8070a5b49d1fa621856d1197
Manon <manonlefkos@gmail.com>
2023-10-23 23:58:51 +0200

    more styling and font change

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ image credits
 "woman holding brown leather bag in bokeh photography" by [Alexandru Zdrobău via Unsplash](https://unsplash.com/photos/woman-holding-brown-leather-bag-in-bokeh-photography-juESZxMhtXk)
 "women's four assorted apparel hanged on clothes rack" by [Alexandra Gorn via Unsplash](https://unsplash.com/photos/womens-four-assorted-apparel-hanged-on-clothes-rack-WF0LSThlRmw)
 "woman holding scarf with cross legs in between of gray concrete wall"[freestocks via Unsplash](https://unsplash.com/photos/woman-holding-scarf-with-cross-legs-in-between-of-gray-concrete-wall-8hAsLeE6Fbo)
+"clothing rack with brown coat" [Chris Reyem via Unsplash](https://unsplash.com/photos/brown-coat-hanged-on-white-plastic-hanger-oJoeGnj8OMM)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
 
   return (
     <>
-      <header className="bg-yellow-600 text-white text-xl">
+      <header className="bg-yellow-600 text-white text-xl font-semibold">
         <div className="max-w-screen-xl flex flex-wrap justify-between p-5 h-16 m-auto gap-2">
-          <div className="italic">FROU FROU</div>
-          <div className="flex gap-8 uppercase">
-            <Link to={`/`}>home</Link>
-            <Link to={`/shop`}>le shop</Link>
+          <div className="italic"><Link to={`/`}>FROU FROU</Link></div>
+          <div className="flex gap-8">
+            <Link to={`/`}>Home</Link>
+            <Link to={`/shop`}>Shop</Link>
           </div>
           <div>
             <Link to={`/cart`}>
               <div>
-                Cart {amountInCart[0]} items for ${amountInCart[1]}
+                Cart: {amountInCart[0]} items ♥ ${amountInCart[1]}
               </div>
             </Link>
           </div>
