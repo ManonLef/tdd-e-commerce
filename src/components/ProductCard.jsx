0a10f30b6b82dbd4b6b3fa8a22ac4acd6447e590
Manon <manonlefkos@gmail.com>
2023-10-23 10:56:17 +0200

    add product cards with styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,22 @@
-const ProductCard = () => {
-  return <div>hi</div>
-}
+const ProductCard = ({ ...props }) => {
+  return (
+    <div className="flex-col border rounded p-3 w-2/12 bg-white">
+      <img src={props.image} alt="" />
+      <div className="flex whitespace-nowrap overflow-hidden">
+        {props.title}
+      </div>
+      <div>$ {props.price}</div>
+      <input
+        type="number"
+        name="amount"
+        id="amount"
+        min="0"
+        className="my-2 px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="1"
+      />
+      <button className="bg-gray-100 px-2 w-full rounded">add to cart</button>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,28 +1,31 @@
-import { useContext } from "react"
-import { ShopContext } from "../App"
+import { useContext } from "react";
+import { ShopContext } from "../App";
 import ProductCard from "./ProductCard";
 
 const Shop = () => {
   // get shop fetched data from Shopping API
   const { data, loading, error } = useContext(ShopContext);
-  
+
   if (error) return <div className="flex-1">error {error}</div>;
   if (loading) return <div className="flex-1">products loading</div>;
-  
-  return (
-    <div>
-      <div>hi from Shop</div>
-      {data.map((product) => {
-        return <ProductCard 
-          key={product.id}
-          title={product.title}
-          description={product.description}
-          image={product.image}
-          price={product.price}
-        />
-      })}
-    </div>
-  )
-}
 
-export default Shop
+  return (
+    <div className="flex-1 p-5">
+      <div className="flex gap-3 flex-wrap justify-center m-10">
+        {data.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              title={product.title}
+              description={product.description}
+              image={product.image}
+              price={product.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
