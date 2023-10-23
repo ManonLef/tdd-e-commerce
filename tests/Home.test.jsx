f6fb59aff1e357ca11585bcb828a908e7745d2fc
Manon <manonlefkos@gmail.com>
2023-10-23 18:25:53 +0200

    adjust test

--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -5,6 +5,6 @@ import Home from "../src/components/Home";
 describe("Shop Component", () => {
   it("renders dummy content", () => {
     render(<Home />)
-    expect(screen.getByText(/hi from home/i)).toBeInTheDocument();
+    expect(screen.getByText(/welcome/i)).toBeInTheDocument();
   })
 })
