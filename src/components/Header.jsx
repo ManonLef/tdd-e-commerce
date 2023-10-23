9bc55a50124e62e77c3b1b0ca9f001b5abdfe5c9
Manon <manonlefkos@gmail.com>
2023-10-23 22:43:16 +0200

    max width styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,20 @@ const Header = () => {
 
   return (
     <>
-      <header className="flex flex-wrap justify-between p-5 h-16  bg-pink-300 text-white">
-        <div>logo</div>
-        <div className="flex gap-8">
-          <Link to={`/`}>home</Link>
-          <Link to={`/shop`}>shop</Link>
-        </div>
-        <div>
-          <Link to={`/cart`}>
-            <div>
-              Cart {amountInCart[0]} items for ${amountInCart[1]}
-            </div>
-          </Link>
+      <header className="  bg-pink-300 text-white">
+        <div className="max-w-screen-xl flex flex-wrap justify-between p-5 h-16 m-auto">
+          <div>logo</div>
+          <div className="flex gap-8">
+            <Link to={`/`}>home</Link>
+            <Link to={`/shop`}>shop</Link>
+          </div>
+          <div>
+            <Link to={`/cart`}>
+              <div>
+                Cart {amountInCart[0]} items for ${amountInCart[1]}
+              </div>
+            </Link>
+          </div>
         </div>
       </header>
     </>
